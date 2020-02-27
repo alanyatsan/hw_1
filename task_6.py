aeb851e5e59c4f95c274fae53df69f3089b6ca17
Анастасия В. Никулина <nikulina_av@edu.kvantorium-pk.ru>
2020-02-27 17:27:38 +1000

    done2

--- task_6.py
+++ task_6.py
@@ -5,7 +5,15 @@ from pyrob.api import *
 
 @task
 def task_5_3():
+    a = False
+    while True:
+        if wall_is_beneath():
+            a = True
+        if not wall_is_beneath() and a:
+            break
+            move_right()
     pass
+
 
 
 if __name__ == '__main__':
